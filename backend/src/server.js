7891a38a6af4d9e8980d641824282e864be36479
farithadnan <22324133+farithadnan@users.noreply.github.com>
2025-04-07 18:17:11 +0800

    feat: set up Express server with middleware and MongoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,30 @@
-import { config } from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import authRoutes from "../src/routes/authRoutes.js";
 
-config();
-console.log(process.env.DB_URI);
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// app.use(cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true,
+// }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
